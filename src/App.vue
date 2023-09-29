6ea6df571ff09d381e9f790c3309919928982c74
Carlos Bernal <ramirez.bernal.jesus1@gmail.com>
2023-09-29 15:48:18 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <user-card :user="userData" />
+  </div>
+</template>
+
+<script>
+import UserCard from "./UserCard.vue";
+
+export default {
+  components: {
+    UserCard
+  },
+  data() {
+    return {
+      userData: {
+        name: "Carlos Bernal",
+        age: 23,
+        status: "Activo"
+      }
+    };
+  }
+};
+</script>
---
+++ src/UserCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="user-card">
+    <div class="card">
+      <div class="card-body">
+        <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style="width:100%">
+        <h5 class="card-title">{{ user.name }}</h5>
+        <p class="card-text">Edad: {{ user.age }}</p>
+        <p class="card-text">Estado: {{ user.status }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.user-card {
+  margin: 20px;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  border-radius: 5px; /* 5px rounded corners */
+}
+
+.card-title {
+  font-size: 1.25rem;
+  margin-bottom: 0.5rem;
+}
+
+.card-text {
+  font-size: 1rem;
+  margin-bottom: 0.25rem;
+}
+/* Add rounded corners to the top left and the top right corner of the image */
+img {
+  max-height: 300px;
+  width: auto;
+  border-radius: 5px 5px 0 0;
+}
+</style>
